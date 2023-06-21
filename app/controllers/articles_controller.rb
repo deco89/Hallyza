649d49d5f5c23edcf1863f6080067cfa6fe7fcd8
Deco89 <decobf15@gmail.com>
2023-06-21 18:15:15 -0300

    CRUD feito parcialmente, faltando o destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,10 +15,15 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    user = User.find_by(id: 1)
-    @article.user = user
+    @article.user = current_user
     @article.save
     redirect_to article_path(@article)
+  end
+
+  def update
+    @article = Article.find(params[:id]) # find the article by id
+    @article.update(article_params) # update the article with the params from the form
+    redirect_to article_path(@article) # redirect to the show page
   end
 
   def destroy
