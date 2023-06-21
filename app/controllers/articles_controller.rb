9b699480d1d1bf3a9bcfde23c421f650cdedaaa8
Deco89 <decobf15@gmail.com>
2023-06-21 18:15:01 -0300

    CRUD feito parcialmente, faltando o destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,31 +1,36 @@
 class ArticlesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
-  
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     @articles = Article.all
   end
 
-  def save
-    @article = Article.new(article_params)
-    @article.save
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
+    user = User.find_by(id: 1)
+    @article.user = user
     @article.save
-    redirect_to articles_path
+    redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    redirect_to articles_path
+    redirect_to article_path
   end
 
   private
 
   # Strong params: white list of sanitized input that can be used to update database records.
   def article_params
-    params.require(:article).permit(:name, :description, :photo)
+    params.require(:article).permit(:name, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get 'articles', to: 'articles#index'
+
+  get 'articles/new', to: 'articles#new'
+
+  get 'articles/:id', to: 'articles#show', as: 'article'
+
+  post 'articles', to: 'articles#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
