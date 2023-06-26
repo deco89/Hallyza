bfd82e8d14a086e0fa2cfe5dadd827adc7e452d3
Deco89 <decobf15@gmail.com>
2023-06-26 19:54:32 -0300

    cards com os artigos na página home funcionando

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :specialties, :institute, :contact ]
 
   def home
+    @articles = Article.all
   end
 
   def specialties
