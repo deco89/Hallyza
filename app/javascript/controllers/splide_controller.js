f8bb7a91a080b9634670651b6a5a5911be50dcbe
Luiz Campedelli <vapeprosper@gmail.com>
2023-07-10 23:47:46 -0300

    added splidejs carousel and stimulusjs controller and styles

---
+++ app/javascript/controllers/splide_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from '@hotwired/stimulus';
+import Splide from '@splidejs/splide';
+
+export default class extends Controller {
+  connect() {
+    const splide = new Splide(this.element, {
+      type: 'loop',
+      perPage: 3,
+      focus  : 'center',
+      autoWidth: true,
+      pauseOnHover: false,
+      interval: 2000,
+    });
+
+    splide.mount();
+  }
+}
