03ee16c4ed44c6e91efb289526fe5447c9c84ecb
Luiz Campedelli <vapeprosper@gmail.com>
2023-07-17 21:39:55 -0300

    fix slide

--- app/javascript/controllers/splide_controller.js
+++ app/javascript/controllers/splide_controller.js
@@ -4,7 +4,7 @@ import Splide from '@splidejs/splide';
 export default class extends Controller {
   connect() {
     const splide = new Splide(this.element, {
-      type: 'loop',
+      // type: 'loop',
       perPage: 3,
       focus  : 'center',
       autoWidth: true,
