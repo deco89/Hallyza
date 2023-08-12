889f238c856bcffc85416951eee771aeb5d04e12
Luiz Campedelli <vapeprosper@gmail.com>
2023-08-12 18:44:54 -0300

    setup seo, meta, favicon primeira parte

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,9 @@ class ApplicationController < ActionController::Base
       unless logged_in?
         redirect_to root_path
       end
-    end
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+end
