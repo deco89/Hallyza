72e011026f17f54f45674905f6464f3c6c71eab3
Deco89 <decobf15@gmail.com>
2023-09-05 18:44:52 -0300

    retirei o carrossel e deixei os artigos em ordem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,6 @@ puts"Creating Users"
 
 hallyza = User.create!(email: "hallyza.rodrigues@gmail.com", password: "Site-1989")
 luiz = User.create!(email: "vapeprosper@gmail.com", password: "Pqp156ldd1")
+andre = User.create!(email: "decobf15@gmail", password: "Main89")
 
 puts"Users Created"
