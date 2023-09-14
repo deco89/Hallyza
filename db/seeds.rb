a1c4c71de3d01d366c728f76113c9c10d784d05e
Deco89 <decobf15@gmail.com>
2023-09-14 11:12:16 -0300

    pequenos ajustes na página inicial

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@ hallyza1 = User.create!(email: "hallyza.rodrigues@hotmail.com", password: "Site-
 hallyza2 = User.create!(email: "hallyza.rodrigues@gmail.com", password: "Contato@4422")
 hallyza3 = User.create!(email: "hallyza.rodrigues@myyahoo.com", password: "Gineco@2023")
 luiz = User.create!(email: "vapeprosper@gmail.com", password: "Pqp156ldd1")
-andre = User.create!(email: "decobf15@gmail", password: "Main89")
+andre = User.create!(email: "decobf15@gmail.com", password: "Main89")
 
 puts"Users Created"
